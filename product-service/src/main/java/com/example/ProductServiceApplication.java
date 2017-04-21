d546daba31feaccaf608fc30847ea76253892d6f
Keith Cummins <keith.cummins.dev@gmail.com>
2017-04-21 16:38:42 +0100

    Inital project microservice

---
+++ product-service/src/main/java/com/example/ProductServiceApplication.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import lombok.Data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+}
+
+@Data
+@Document(collection = "products")
+class Product {
+	@Id
+	private String id;
+	private String name;
+	private double price;
+}
+
+@RepositoryRestResource
+interface ProductRepository extends MongoRepository<Product, String> {
+
+}
