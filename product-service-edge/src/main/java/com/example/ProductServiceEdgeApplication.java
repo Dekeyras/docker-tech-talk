2947c3ea70e1d1b08d0445edd3015bc13c41a109
Keith Cummins <keith.cummins.dev@gmail.com>
2017-04-24 14:29:31 +0100

    Added circuit breaker

--- product-service-edge/src/main/java/com/example/ProductServiceEdgeApplication.java
+++ product-service-edge/src/main/java/com/example/ProductServiceEdgeApplication.java
@@ -1,9 +1,11 @@
 package com.example;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignClient;
@@ -13,10 +15,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
 import static java.util.stream.Collectors.toList;
 
+@EnableCircuitBreaker
 @EnableZuulProxy
 @EnableDiscoveryClient
 @EnableFeignClients
@@ -38,6 +43,11 @@ class ProductApi {
         this.productResource = productResource;
     }
 
+    public Collection<String> fallback() {
+        return Arrays.asList("Product 1");
+    }
+
+    @HystrixCommand(fallbackMethod = "fallback")
     @GetMapping("/names")
     public Collection<String> names() {
         return this.productResource.getAll()
